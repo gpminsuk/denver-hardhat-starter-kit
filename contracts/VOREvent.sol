2fa30ff9067930c4ebba2a93bd997add44d2681b
gpminsuk <gpminsuk@gmail.com>
2023-03-01 00:20:29 -0700

    Add test

---
+++ contracts/VOREvent.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract VOREvent is ERC721 {
+    enum EventState {
+        PLANNED,
+        STARTED,
+        ENDED
+    }
+
+    enum BadgeState {
+        NONE,
+        UNASSIGNED,
+        SENT,
+        ACCEPTED,
+        REJECTED
+    }
+
+    struct Badge {
+        BadgeState state;
+        address recipient;
+        string name;
+        string description;
+    }
+
+    mapping(uint256 => Badge) private badges;
+
+    string public description;
+    address public issuer;
+    EventState public state;
+
+    constructor(
+        string memory _name,
+        string memory _description,
+        address _issuer
+    ) ERC721(_name, "VORB") {
+        description = _description;
+        issuer = _issuer;
+        state = EventState.PLANNED;
+    }
+
+    function addBadge(
+        string memory _name,
+        string memory _description,
+        uint256 _tokenId
+    ) public {
+        require(msg.sender == issuer, "Only organizer can add badge");
+        require(state == EventState.PLANNED, "Event is already started");
+        Badge memory badge = Badge({
+            state: BadgeState.UNASSIGNED,
+            recipient: address(0),
+            name: _name,
+            description: _description
+        });
+        badges[_tokenId] = badge;
+        _safeMint(msg.sender, _tokenId);
+    }
+
+    function removeBadge(uint256 _tokenId) public {
+        require(msg.sender == issuer, "Only organizer can remove badge");
+        require(state == EventState.PLANNED, "Event is already started");
+        require(badges[_tokenId].state != BadgeState.NONE, "Badge not found");
+        _burn(_tokenId);
+        Badge memory zero;
+        badges[_tokenId] = zero;
+    }
+
+    function award(address _to, uint256 _tokenId) public {
+        require(msg.sender == issuer, "Only organizer can award badge");
+        require(_to != issuer, "Cannot award badge to organizer");
+        require(state == EventState.ENDED, "Event is not ended yet");
+        require(
+            badges[_tokenId].state == BadgeState.UNASSIGNED,
+            "Badge is in invalid state"
+        );
+        badges[_tokenId].recipient = _to;
+        badges[_tokenId].state = BadgeState.SENT;
+    }
+
+    function accept(uint256 _tokenId) public {
+        require(
+            msg.sender == badges[_tokenId].recipient,
+            "Only recipient can accept badge"
+        );
+        require(
+            badges[_tokenId].state == BadgeState.SENT,
+            "Badge is in invalid state"
+        );
+        badges[_tokenId].state = BadgeState.ACCEPTED;
+        _safeTransfer(issuer, msg.sender, _tokenId, "");
+    }
+
+    function reject(uint256 _tokenId) public {
+        require(
+            msg.sender == badges[_tokenId].recipient,
+            "Only recipient can reject badge"
+        );
+        require(
+            badges[_tokenId].state == BadgeState.SENT,
+            "Badge is in invalid state"
+        );
+        badges[_tokenId].state = BadgeState.REJECTED;
+    }
+
+    function startEvent() public {
+        require(msg.sender == issuer, "Only organizer can start event");
+        require(
+            state == EventState.PLANNED,
+            "Only planned event can be started"
+        );
+        state = EventState.STARTED;
+    }
+
+    function endEvent() public {
+        require(msg.sender == issuer, "Only organizer can start event");
+        require(state == EventState.STARTED, "Only started event can be ended");
+        state = EventState.ENDED;
+    }
+
+    function _beforeTokenTransfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal override {
+        require(
+            // burn
+            (_to == address(0) &&
+                _from == issuer &&
+                badges[_tokenId].state == BadgeState.UNASSIGNED) ||
+                // mint
+                (_from == address(0) &&
+                    _to == issuer &&
+                    badges[_tokenId].state == BadgeState.UNASSIGNED) ||
+                // award
+                (_to != issuer &&
+                    _from == issuer &&
+                    state == EventState.ENDED &&
+                    badges[_tokenId].state == BadgeState.ACCEPTED),
+            "Invalid transfer"
+        );
+        super._beforeTokenTransfer(_from, _to, _tokenId);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://ipfs.io/ipfs/";
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(_exists(_tokenId), "URI query for nonexistent token");
+        string memory currentBaseURI = _baseURI();
+        return
+            string(
+                abi.encodePacked(currentBaseURI, Strings.toString(_tokenId))
+            );
+    }
+}
---
+++ test/contracts/VOREvent.spec.js
@@ -0,0 +1,149 @@
+const { network, ethers } = require("hardhat");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { developmentChains } = require("../../helper-hardhat-config");
+const { expect } = require("chai");
+
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("VOREvent unit test", async function () {
+      async function deployContract() {
+        const [deployer] = await ethers.getSigners();
+        const factory = await ethers.getContractFactory("VOREvent");
+        const contract = await factory.deploy(
+          "Chess tournament",
+          "The Bear Creek school 2nd grade chess tournament",
+          deployer.address
+        );
+        await contract.deployed();
+        return contract;
+      }
+
+      it("Test VOREvent contract", async () => {
+        const [deployer, recepient1, recepient2] = await ethers.getSigners();
+        const contract = await loadFixture(deployContract);
+
+        // cannot award badges before the event ends
+        await expect(contract.award(recepient1.address, 1)).to.be.revertedWith(
+          "Event is not ended yet"
+        );
+
+        // can add badges
+        await contract.addBadge("1st place", "Prize for the first place", 1);
+        await contract.addBadge("2nd place", "Prize for the second place", 2);
+        await contract.addBadge("3rd place", "Prize for the third place", 3);
+
+        // cannot remove badge that does not exist
+        await expect(contract.removeBadge(4)).to.be.revertedWith(
+          "Badge not found"
+        );
+
+        // can remove badge that exists
+        await contract.removeBadge(3);
+
+        // cannot end event before started
+        await expect(contract.endEvent()).to.be.revertedWith(
+          "Only started event can be ended"
+        );
+
+        // start the event
+        await contract.startEvent();
+
+        // cannot start event twice
+        await expect(contract.startEvent()).to.be.revertedWith(
+          "Only planned event can be started"
+        );
+
+        // cannot add badges after the event starts
+        await expect(
+          contract.addBadge("3rd place", "Prize for the third place", 3)
+        ).to.be.revertedWith("Event is already started");
+
+        // cannot remove badges after the event starts
+        await expect(contract.removeBadge(3)).to.be.revertedWith(
+          "Event is already started"
+        );
+
+        // cannot award badges before the event ends
+        await expect(contract.award(recepient1.address, 1)).to.be.revertedWith(
+          "Event is not ended yet"
+        );
+
+        // end the event
+        await contract.endEvent();
+
+        // cannot end event twice
+        await expect(contract.endEvent()).to.be.revertedWith(
+          "Only started event can be ended"
+        );
+
+        // cannot start ended event again
+        await expect(contract.startEvent()).to.be.revertedWith(
+          "Only planned event can be started"
+        );
+
+        // cannot add badges after the event starts
+        await expect(
+          contract.addBadge("3rd place", "Prize for the third place", 3)
+        ).to.be.revertedWith("Event is already started");
+
+        // cannot remove badges after the event starts
+        await expect(contract.removeBadge(3)).to.be.revertedWith(
+          "Event is already started"
+        );
+
+        // cannot award badges to organizer
+        await expect(contract.award(deployer.address, 1)).to.be.revertedWith(
+          "Cannot award badge to organizer"
+        );
+
+        // award badges
+        await contract.award(recepient1.address, 1);
+        await contract.award(recepient1.address, 2);
+
+        // cannot award a badge twice
+        await expect(contract.award(recepient2.address, 2)).to.be.revertedWith(
+          "Badge is in invalid state"
+        );
+
+        // cannot award a badge that does not exist
+        await expect(contract.award(recepient2.address, 3)).to.be.revertedWith(
+          "Badge is in invalid state"
+        );
+
+        // cannot accept if not awarded
+        await expect(contract.accept(1)).to.be.revertedWith(
+          "Only recipient can accept badge"
+        );
+
+        // cannot reject if not awarded
+        await expect(contract.reject(1)).to.be.revertedWith(
+          "Only recipient can reject badge"
+        );
+
+        // accept badge 1
+        await contract.connect(recepient1).accept(1);
+
+        // cannot accept accepted badge
+        await expect(contract.connect(recepient1).accept(1)).to.be.revertedWith(
+          "Badge is in invalid state"
+        );
+
+        // cannot reject accepted badge
+        await expect(contract.connect(recepient1).reject(1)).to.be.revertedWith(
+          "Badge is in invalid state"
+        );
+
+        // reject reward 2
+        await contract.connect(recepient1).reject(2);
+
+        // cannot accept rejected badge
+        await expect(contract.connect(recepient1).accept(2)).to.be.revertedWith(
+          "Badge is in invalid state"
+        );
+
+        // cannot reject rejected badge
+        await expect(contract.connect(recepient1).reject(2)).to.be.revertedWith(
+          "Badge is in invalid state"
+        );
+      });
+    });
